6b2158d24a54c2f689ea2e5f0b6551b83f3d48b7
Daiki Kishi <dik.kishi.1108@gmail.com>
2021-06-16 14:14:43 +0900

    index.htmlを読み込んで表示するよう、index.jsを変更

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
+const fs = require('fs');
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+    fs.readFile('./index.html', 'UTF-8', (error, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 });
 const port = 8000;
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+    console.log('Listining on ' + port);
 });
