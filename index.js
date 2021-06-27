a98f4eddc9b7b2d059da79fad985ab7c49db8d37
furuike <cactus0411.d@gmail.com>
2021-06-27 02:05:45 +0000

    HTMLの表示

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+  fs.readFile('index.html', 'utf8', (err, data) => {
+    if(err){
+      res.writeHead(404,{
+        'Content-Type': 'text/plain; charset=utf-8'
+      });
+      res.write('Server Error!');
+      res.end();
+    }else{
+      res.writeHead(200,{
+        'Content-Type': 'text/html; charset=utf-8'
+      });
+      res.write(data);
+      res.end();
+    }
+  })
+  
 });
-const port = 8000;
+const port = 8080;
 server.listen(port, () => {
   console.log('Listening on ' + port);
 });
