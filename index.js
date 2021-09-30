0fff64e3cd64c303f3ccf485b004b0bd49f32e65
Happy Kumamoto <zen.word+hub@gmail.com>
2021-09-30 12:10:39 +0900

    htmlでapple

--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(
+    `<!DOCTYPE html><html lang="ja"><body>
+     <h1 style="text-align:center;">
+     <span style="color:lightgreen; display:inline-block; transform:rotate(-45deg); margin-top:50px;">
+     &nbsp; light</span><br>
+     <span style="color:lightgreen;">greenlightgreen</span><br>
+     <span style="color:gold;">goldenyellow &nbsp; &nbsp; &nbsp;</span><br> 
+     <span style="color:orange;">orangeorange &nbsp; &nbsp; &nbsp; &nbsp;</span><br> 
+     <span style="color:red;">redredredred &nbsp; &nbsp; &nbsp; &nbsp;</span><br> 
+     <span style="color:purple;">purplepurple</span><br> 
+     <span style="color:blue;">blue &nbsp; blue</span>  `
+  );
   res.end();
 });
 const port = 8000;
