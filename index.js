aa29a65e5e7bfd81562dc94aa7f709ba3bcfea14
nyeoro-bong <68091561+nyeoro-bong@users.noreply.github.com>
2021-10-01 07:17:09 +0900

    Dockerでサーバー実装

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(`
+    <html lang="ja">
+        <body>
+          <h1>わーい！Dockerでサーバー立てが楽ちん！</h1> 
+        </body>
+    </html>
+    `
+  );
   res.end();
 });
 const port = 8000;
