a1777e32dcd8ce53d54df8f8b92a205baf703ed6
chikako2020 <smkdualclean-3@yahoo.co.jp>
2021-10-01 20:13:40 +0900

    HTTP サーバーを改良してHTMLを表示する

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(
+    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1><h3>今日は金曜日</h3><h5>明日は運動会</h5></body></html>'
+  );
   res.end();
 });
 const port = 8000;
