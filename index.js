1623b1e196fe41657f476f8c778c4eced2d4fe0c
aoi nakatsu <hokugin12@gmail.com>
2021-10-01 20:20:52 +0900

    htmlを表示する

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 const http = require('http');
+
+//サーバー設定
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+  res.writeHead(200, {　//200=>正常終了
+    'Content-Type': 'text/html; charset=utf-8'　//返すデーターの種類
   });
-  res.write(req.headers['user-agent']);
-  res.end();
+  res.write(
+    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
+ );
+  res.end();　//レスポンスの確定
 });
 const port = 8000;
 server.listen(port, () => {
