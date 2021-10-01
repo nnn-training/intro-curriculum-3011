c1f2767aa836badf1941cf7c5fc0633027b790ae
Fuuchin <denkisutandokese@outlook.jp>
2021-10-01 19:38:57 +0900

    HTTPサーバーを立てました

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+    // ファイルを読み込んだら、コールバック関数を実行する。
+    fs.readFile('./index.html', 'utf-8' , doReard );
+    
+    // コンテンツを表示する。
+    function doReard(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    }
 });
 const port = 8000;
 server.listen(port, () => {
