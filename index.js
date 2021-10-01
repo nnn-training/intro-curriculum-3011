e69884344fe85adc54a0e95359064875abf713a6
fleeting-moment <79242345+fleeting-moment@users.noreply.github.com>
2021-10-01 17:55:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8;test/css'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><style>h1{color:red}</style><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
   res.end();
 });
 const port = 8000;
