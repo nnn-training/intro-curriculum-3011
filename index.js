e3c62b9e809f03d907425dc685851350c7b69978
吉田稲作 <yoshidanoinasaku@gmail.com>
2021-11-15 18:49:15 +0900

    htmlファイルの読み込み

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+  fs.readFile('./index.html', 'utf8', doResponse);
+
+  function doResponse(err, data) {
+    res.writeHead(200, {
+      'Content-Type' : 'text/html; charset=utf-8'
+    });
+    res.write(data);
+    res.end();
+  }
 });
 const port = 8000;
 server.listen(port, () => {
