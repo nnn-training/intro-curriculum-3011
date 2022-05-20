1894775b8ec76c44afebeeacef84d3fe3e3ed557
kobucha <kashikomi@outlook.com>
2022-05-20 12:46:47 +0900

    修正

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   <!DOCTYPE html>
   <html lang="ja">
     <body>
-      <h1>HTMLの一番大きい見出しを表示します</h1>
+      <h1>[修正]HTMLの一番大きい見出しを表示します</h1>
     </body>
   </html>`);
   res.end();
