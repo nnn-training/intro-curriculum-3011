3a36e5750a9c5ddfe70e469ad0962cdcaefa5d48
aaa <143986683+tsu3n@users.noreply.github.com>
2024-03-05 20:09:48 +0900

    readFileで読み込むように変更

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 const http = require('node:http');
+const fs = require('node:fs');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
-  });
-  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
-  res.end();
+  fs.readFile('./index.html', 'utf-8', (err,data)=> { 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  })
 });
 const port = 8000;
 server.listen(port, () => {
