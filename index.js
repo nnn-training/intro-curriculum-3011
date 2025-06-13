fa353a28b80da0d91b88a764ce30aedf19dbb17b
AcchonBurike <AcchonBurike@example.com>
2025-06-13 11:54:14 +0900

    HTTPサーバがHTMLを返すように変更

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 'use strict';
+
+const fs = require('node:fs');
+
 const http = require('node:http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+
+const filePath = './index.html';
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    res.writeHead(404);
+    res.end('File not found');
+    return;
+  }
+  res.write(data);
   res.end();
+  })
 });
+
 const port = 8000;
 server.listen(port, () => {
   console.log(`Listening on ${port}`);
