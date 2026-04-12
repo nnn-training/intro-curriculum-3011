94631f151ec38470961da48a268c525f716c24fd
0xnorii <111930238+0xnorii@users.noreply.github.com>
2026-04-12 21:45:22 +0900

    HTMLを返すように変更

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 const http = require('node:http');
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+    'Content-Type': 'text/html; charset=utf-8'
+  });  // ← まずwriteHeadを閉じる
+  res.write(
+    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
+  );
+  res.end();  // ← これも必要
 });
-const port = 8000;
-server.listen(port, () => {
-  console.log(`Listening on ${port}`);
-});
+
+server.listen(8000);
