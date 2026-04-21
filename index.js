8a30f063153ec4aa23d8e5499c03a997e2402787
Chiie08 <chie_0824_k@yahoo.co.jp>
2026-04-21 16:29:57 +0200

    HTMLを返すように変更

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-'use strict';
-const http = require('node:http');
+"use strict";
+const http = require("node:http");
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    "Content-Type": "text/html; charset=utf-8",
   });
-  res.write(req.headers['user-agent']);
+
+  res.write(`
+    <!DOCTYPE html>
+    <html lang="ja">
+      <body>
+        <h1>HTMLの一番大きい見出しを表示します</h1>
+      </body>
+    </html>
+  `);
+
   res.end();
 });
+
 const port = 8000;
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
   console.log(`Listening on ${port}`);
 });
